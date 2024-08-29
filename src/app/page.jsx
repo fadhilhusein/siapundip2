85c1da925e5b2ea51e234224d9257621a733be2e
M Fadhil Syahrian <muhammad.fadhil.syahrian@gmail.com>
2024-08-29 16:46:07 +0700

    siapundip version 1

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <img src="assets/unnamed.webp" alt="siapundip.apk"/>
+      <a href="./SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk" download="SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk">Download SiapUndip (APK)</a>
+      <footer>
+          <p>Made with <span>♥</span> By Muhammad Fadhil Syahrian - Manajemen 24</p>
+      </footer>
+    </>
+  );
+}
