db5825e98d924a6efe5c38c0cf4d5e74a4bb204b
M Fadhil Syahrian <muhammad.fadhil.syahrian@gmail.com>
2024-08-29 16:49:35 +0700

    update link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <img src="assets/unnamed.webp" alt="siapundip.apk"/>
-      <a href="./SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk" download="SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk">Download SiapUndip (APK)</a>
+      <a href="assets/SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk" download="SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk">Download SiapUndip (APK)</a>
       <footer>
           <p>Made with <span>♥</span> By Muhammad Fadhil Syahrian - Manajemen 24</p>
       </footer>
