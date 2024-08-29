a061dd0919389a641c7606ed4dafcb3a542f1144
M Fadhil Syahrian <muhammad.fadhil.syahrian@gmail.com>
2024-08-29 17:02:50 +0700

    update tutorial

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "SiapUndip",
   description: "Download Siap Undip APK",
+  icons: {
+    icon: "next.svg"
+  }
 };
 
 export default function RootLayout({ children }) {
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,14 @@ export default function Home() {
     <>
       <img src="assets/unnamed.webp" alt="siapundip.apk"/>
       <a href="assets/SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk" download="SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk">Download SiapUndip (APK)</a>
+      <p>Cara install SiapUndip (ANDROID)</p>
+      <ol>
+        <li>Download APK Siap diatas ^^^</li>
+        <li>Download "ZipArchiver" di Playstore</li>
+        <li>Buka ZipArchiver dan buka folder download</li>
+        <li>Pencet file dengan nama "SIAP UNDIP MAHASISWA_2.1.9_APKPure.xapk" </li>
+        <li>Pilih menu install dan Siap telah terinstall <span>♥</span></li>
+      </ol>
       <footer>
           <p>Made with <span>♥</span> By Muhammad Fadhil Syahrian - Manajemen 24</p>
       </footer>
